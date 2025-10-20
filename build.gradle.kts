5106662e444f3fd69c1dba5154f40efc218f0677
Jordon de Hoog <jordon.dehoog@gmail.com>
2025-10-20 17:26:03 -0400

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.dokka.gradle.AbstractDokkaTask
-import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
-
 plugins {
     alias(libs.plugins.multiplatform) apply false
     alias(libs.plugins.compose) apply false
@@ -20,22 +17,13 @@ apiValidation {
     )
 }
 
-tasks.withType<DokkaMultiModuleTask>().configureEach {
-    outputDirectory.set(rootDir.resolve("dokka"))
-}
-
-// TODO: Is this still needed?
-allprojects {
-    // Workaround for https://github.com/Kotlin/dokka/issues/2977.
-    // We disable the C Interop IDE metadata task when generating documentation using Dokka.
-    tasks.withType<AbstractDokkaTask> {
-        @Suppress("UNCHECKED_CAST")
-        val taskClass = Class.forName(
-            "org.jetbrains.kotlin.gradle.targets.native.internal" +
-                ".CInteropMetadataDependencyTransformationTask"
-        ) as Class<Task>
-        parent?.subprojects?.forEach { project ->
-            dependsOn(project.tasks.withType(taskClass))
-        }
-    }
+dependencies {
+    dokka(projects.core)
+    dokka(projects.dispatcher)
+    dokka(projects.dispatcherCompose)
+    dokka(projects.extensionsAndroid)
+    dokka(projects.extensionsCompose)
+    dokka(projects.extensionsCompose)
+    dokka(projects.extensionsViewmodel)
+    dokka(projects.extensionsVoyager)
 }
